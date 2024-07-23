bed9490d42ac65df8d003167bbc1758e20218593
Danilo Cimesa <102156507+DaniloCimesa@users.noreply.github.com>
2024-07-23 13:29:37 +0200

    Update W3-resources-Vol1.py

--- W3-resources-Vol1.py
+++ W3-resources-Vol1.py
@@ -168,3 +168,81 @@ def NumCheck():
 
 #excercise17
 
+def function3(n):
+    return abs(n-1000)<=100 or abs(n-2000)<=100
+
+#excercise18
+
+def functionsum():
+    a=input('Input first number:')
+    b=input('Input second number:')
+    c=input('Input third number:')
+    a1=int(a)
+    b1=int(b)
+    c1=int(c)
+    if a1==b1==c1:
+        return 3*(a1+b1+c1)
+    else:
+        return a1+b1+c1
+    
+
+functionsum()
+
+#excercise19
+
+def addIs (text):
+    if text[:2]=='Is':
+        return text
+    else:
+        return 'Is'+text
+    
+#excercise20
+
+def returnco(string, n):
+    return string*n
+
+print(returnco('Branko',2))
+
+#excercise21
+
+def EvenOrOdd ():
+    try:
+        numb=int(input('Input number:'))
+        if numb%2==0:
+            print('Number is even!')
+        else:
+            print('Number is odd!')
+    except ValueError:
+        print('Num is not int')
+        
+#excercise22
+
+def num_4_count(n):
+
+    count=0
+
+    for i in n:
+        if i==4:
+            count=count+1
+
+    return count
+
+#excercise23
+
+def func_string(text,n):
+    minlen=2
+    if len(text)<minlen:
+        return text*n
+    else:
+        return text[:2]*n
+    
+#excercise24
+
+def Vowel():
+    n=input('Input letter:')
+    vowels=('a','e','i','o','u')
+    if n in vowels:
+        return True
+    else:
+        return False
+
